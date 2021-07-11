8baaca115d2685f7a093b930fdd856524be52fc1
Ben Silveston <bsilveston37@gmail.com>
2021-07-11 13:54:17 +0100

    Added the current meetup list dynamically to the 'Favourites' page.

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,24 @@
-function Favourites() {
-	return <div>Favourites Page</div>;
-}
+import { useContext } from "react";
 
+import FavouritesContext from "../store/favourites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
+function Favourites() {
+	const favouritesContext = useContext(FavouritesContext);
+
+	let content;
+
+	if (favouritesContext.totalFavourites === 0) {
+		content = <p>You currently have no favourite meetups.</p>;
+	} else {
+		content = <MeetupList meetups={favouritesContext.favourites} />;
+	}
+
+	return (
+		<section>
+			<h1>My Favourites</h1>
+			{content}
+		</section>
+	);
+}
 export default Favourites;
